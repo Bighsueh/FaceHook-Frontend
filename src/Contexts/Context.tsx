7ebdff94b8f6077386247be3957e599aea59b74c
TINGYUNLI <rita891013@gmail.com>
2023-10-13 12:50:27 +0800

    add user context

--- src/Contexts/Context.tsx
+++ src/Contexts/Context.tsx
@@ -1,16 +1,39 @@
-import { PropsWithChildren, createContext, useState } from 'react';
+import { PropsWithChildren, createContext, useState, useEffect } from 'react';
+import AuthService from '../API/Auth'
 
 type ContextType = {
   theme: string;
   setTheme: (theme: string) => void;
+  content: string; 
+  setContent: (content: string) => void;
+  currentUser: any;
+  setCurrentUser: (user: any) => void;
 };
+
 export const Context = createContext<ContextType | undefined>(undefined);
 
-export const ThemeProvider = ({ children }: PropsWithChildren<{}>) => {
+export const ContextProvider = ({ children }: PropsWithChildren<{}>) => {
   const [theme, setTheme] = useState<ContextType['theme']>('');
+  const [content, setContent] = useState<ContextType['content']>('');
+  const [currentUser, setCurrentUser] = useState<ContextType['currentUser']>('');
+
+  useEffect(() => {
+    const fetchUserInfo = async () => {
+      try {
+        const response = await AuthService.getCurrentUser();
+        const user = response.data;
+        setCurrentUser(user);
+      } catch (error) {
+        console.error('無法獲取使用者資訊', error);
+      }
+    };
+
+    fetchUserInfo();
+  }, []);
+
 
   return (
-    <Context.Provider value={{ theme, setTheme }}>
+    <Context.Provider value={{ theme, setTheme, content, setContent,currentUser, setCurrentUser }}>
       {children}
     </Context.Provider>
   );
