d1fa39aca77466b89996ff3d852689b1f23de90b
TINGYUNLI <rita891013@gmail.com>
2023-11-21 01:08:37 +0800

    modify post layout

--- src/Page/Home.tsx
+++ src/Page/Home.tsx
@@ -1,18 +1,16 @@
 import React from 'react';
-import Post from '../Component/Post';
 import Feed from '../Component/Feed';
-
+import CreatePost from '../Component/createPost';
 
 function Home() {
   return (
 
-    <div className="ml-12 pl-12">
-      <div className='mt-10 ml-12'>
-          <Post />
-          <Feed />
+    <div className='grid grid-cols-12 mt-10 ml-12'>
+      <div className="flex-row row-start-1 col-span-7 col-start-2 space-y-4">
+        <CreatePost/>
+        <Feed />
       </div>
     </div>
-
 
   )
 }
