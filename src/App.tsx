5b5c083ea3496057a99b8f30d7a355390e290704
TINGYUNLI <rita891013@gmail.com>
2023-11-21 01:03:53 +0800

    加入搜尋貼文功能

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,9 @@ import Login from './Page/Login';
 import Layout1 from './Page/Layout/layout1';
 import Layout2 from './Page/Layout/layout2';
 import { ContextProvider, Context } from './Contexts/Context';
+import Search from './Page/Search';
+import Personal from './Page/Personal';
+import Friend from './Page/Friend';
 
 
 function App() {
@@ -37,11 +40,21 @@ function App() {
             {(currentUser !== null && currentUser !== '') ? (
               <Route path="/" element={<Layout1 />} >
                 <Route index element={<Home />} />
-                <Route path="/home" element={<Home />} />
                 <Route path="/profile" element={<Profile/>} />
+                <Route path="/search" element={<Search/>} />
               </Route>
               ):(
               <Route path="/" element={<Layout2 />}>
+                <Route index element={<Login />} />
+              </Route>
+            )}
+            {(currentUser !== null && currentUser !== '') ? (
+              <Route path="/personal/:userId" element={<Layout2 />}>
+                <Route path="/personal/:userId" element={<Personal/>} />
+                <Route path="/personal/:userId/friend" element={<Friend/>} />
+              </Route>
+            ):(
+              <Route path="/home" element={<Layout2 />}>
                 <Route index element={<Login />} />
               </Route>
             )}
