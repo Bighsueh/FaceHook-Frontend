ec591c8ed3ffd86b95ae1be9c7ddfe827d3bc6fb
bighsueh <ss313021490@gmail.com>
2023-12-06 14:54:22 +0800

    將 ChatContextProvder 套用到主頁面

--- src/Page/Layout/layout1.tsx
+++ src/Page/Layout/layout1.tsx
@@ -4,31 +4,32 @@ import Navbar from '../../Component/Navbar';
 import Sidebox from '../../Component/Sidebox';
 import RightSideBar from '../../Component/RightSideBar';
 import ChatRoom from '../../Component/ChatRoom';
+import { ChatContextProvider } from '../../Contexts/ChatContext';
 
 function Layout1() {
   return (
-    <>
-    <div className='flex flex-col h-screen bg-white'>
-      <div className='fixed top-0 left-0 right-0 z-10'>
-        <Navbar/>
+    <ChatContextProvider>
+      <div className='flex flex-col h-max bg-white'>
+        <div className='fixed top-0 left-0 right-0 z-10'>
+          <Navbar/>
+        </div>
+        <div className='flex flex-row'>
+          <div className='w-1/6 h-screen fixed top-16 left-0'>
+            <Sidebox/>
+          </div>
+          <div className='flex-grow pl-16 ml-10 pt-16 mt-5 w-3/6'>
+              <div className='pl-16 ml-16'>
+                <Outlet />
+              </div>
+          </div>
+          <div className='w-1/6 h-screen fixed top-16 right-0'>
+            <RightSideBar/>
+          </div>
+      
+        </div>
       </div>
-      <div className='flex flex-row'>
-        <div className='w-1/6 h-screen fixed top-16 left-0'>
-          <Sidebox/>
-        </div>
-        <div className='flex-grow pl-16 ml-10 pt-16 mt-5 w-3/6'>
-            <div className='pl-16 ml-16'>
-              <Outlet />
-            </div>
-        </div>
-        <div className='w-1/6 h-screen fixed top-16 right-0'>
-          <RightSideBar/>
-        </div>
-    
-      </div>
-    </div>
-    <ChatRoom/>
-    </>
+      <ChatRoom/>
+    </ChatContextProvider>
   )
 }
 
